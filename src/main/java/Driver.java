d597bd98ea8056be5d86576aa4cd05acce863f29
Danny <tryon.dan@gmail.com>
2018-10-19 12:35:51 -0700

    Set up packages

---
+++ src/main/java/Driver.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Driver {
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/tracer/SequenceLogger.java
@@ -0,0 +1,4 @@
+package tracer;
+
+public class SequenceLogger {
+}
