1e7b0fa74347751404fb045db367a0d908e1eb54
Danny <tryon.dan@gmail.com>
2018-10-20 08:15:47 -0700

    Update trace log function to handle multiple input types

--- src/main/java/tracer/SequenceLogger.java
+++ src/main/java/tracer/SequenceLogger.java
@@ -1,16 +1,28 @@
 package tracer;
 
+import com.sun.deploy.trace.Trace;
 import tracer.cursor.CursorSelectController;
 import tracer.cursor.interfaces.CursorSelectListener;
+import tracer.database.TraceTable;
 import tracer.database.UserTable;
 import tracer.database.Database;
 import tracer.database.ElephantDatabase;
+import tracer.database.exceptions.LogEntryException;
+import tracer.database.interfaces.TraceModel;
 import tracer.database.interfaces.UserModel;
 import tracer.login.LoginController;
 import tracer.login.interfaces.LoginListener;
 import tracer.login.view.View;
+import tracer.sequence.TraceFormatter;
 
 import javax.swing.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import java.awt.*;
+import java.util.Date;
+import java.util.Random;
 
 public class SequenceLogger implements LoginListener, CursorSelectListener
 {
@@ -23,6 +35,7 @@ public class SequenceLogger implements LoginListener, CursorSelectListener
     private LoginController login;
     private CursorSelectController cursorSelector;
 
+    private TraceTable traceTable;
 
 
     public SequenceLogger()
@@ -42,8 +55,7 @@ public class SequenceLogger implements LoginListener, CursorSelectListener
          cursorSelector.addListener(this);
 
         // Trace table within database for use with trace logging
-        // TODO: create trace model
-        // TraceModel traceModel = new DBTraceModel(database);
+        traceTable = new TraceTable(database);
 
 
 
@@ -73,5 +85,62 @@ public class SequenceLogger implements LoginListener, CursorSelectListener
         System.out.println("Sequence Logger: " + "Cursor Type Selected: " + this.cursor_type);
 
         // Start sequence tracker
-    }
+
+//        ArrayList<Point> pointArrayList = new ArrayList<Point>();
+//        ArrayList<Integer> timesMS = new ArrayList<Integer>();
+//
+//        Random rng = new Random();
+//
+//        for (int i = 0; i < 100; ++i)
+//        {
+//            pointArrayList.add(new Point(rng.nextInt(100), rng.nextInt(100)));
+//            timesMS.add(rng.nextInt(10));
+//        }
+//
+//        System.out.println("num points before norm: " + pointArrayList.size());
+//
+//        // TODO: test
+//        pointArrayList = TraceFormatter.normalizeTime(pointArrayList, timesMS, 0.01);
+//
+//        System.out.println("num points after norm: " + pointArrayList.size());
+//
+//
+//        String pointString = TraceFormatter.getPoints(pointArrayList);
+//
+//        Date date = new Date();
+//        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//        String dateString = dateFormat.format(date);
+//
+//        String[] traceValues = new String[] {
+//                username,
+//                "default",
+//                dateString,
+//                pointString,
+//                "5x5x0000",
+//                "10",
+//                "11",
+//                "0",
+//                cursor_type,
+//                "0.01"
+//        };
+//        try {
+//            traceTable.logTrace(traceValues);
+//
+//            traceTable.logTrace(
+//                    username,
+//                    pointArrayList,
+//                    "5x9x1093",
+//                    66,
+//                    99,
+//                    100,
+//                    cursor_type,
+//                    0.09
+//            );
+//
+//        }
+//        catch (LogEntryException ex) {
+//            ex.printStackTrace();
+//        }
+//
+//    }
 }
--- src/main/java/tracer/database/TraceTable.java
+++ src/main/java/tracer/database/TraceTable.java
@@ -2,17 +2,32 @@ package tracer.database;
 
 import tracer.database.exceptions.LogEntryException;
 import tracer.database.interfaces.TraceModel;
+import tracer.sequence.TraceFormatter;
 
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+import java.awt.*;
 
 public class TraceTable implements TraceModel {
 
-    private String usersTable = "users";
-    private String[] usersTableFields = {"username", "password", "first_name", "last_name"};
-    private String[] usersTableFieldsPartial = {"username", "password"};
+    private String tracesTable = "traces";
 
-    private String strokesTable = "strokes";
-    private String[] strokesTableFields = {"username", "direction", "points", "id"};
+    private String[] tracesTableFields = {
+            "username",
+            "id",
+            "date",
+            "points",
+            "sequence_id",
+            "position",
+            "round",
+            "attempt",
+            "cursor_type",
+            "poll_rate"
+    };
 
     private Database database = null;
 
@@ -20,9 +35,59 @@ public class TraceTable implements TraceModel {
         database = db;
     }
 
-    public void logEntry(String table, String[] values) throws LogEntryException {
+    public void logEntry(String table, String[] values) throws LogEntryException
+    {
         try {
             database.insertIntoTable(table, values);
+        }
+        catch (SQLException ex) {
+            LogEntryException.throwLogFailedException();
+        }
+    }
+
+    public void logTrace(String[] values) throws LogEntryException
+    {
+        try {
+            database.insertIntoTable(tracesTable, values);
+        }
+        catch (SQLException ex) {
+            LogEntryException.throwLogFailedException();
+        }
+    }
+
+    public void logTrace(
+            String username,
+            ArrayList<Point> points,
+            String sequence_id,
+            int position,
+            int round,
+            int attempt,
+            String cursor_type,
+            double poll_rate
+    ) throws LogEntryException
+    {
+
+        String pointString = TraceFormatter.getPoints(points);
+
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = dateFormat.format(date);
+
+        String[] values = new String[] {
+                username,
+                "default",
+                dateString,
+                pointString,
+                sequence_id,
+                Integer.toString(position),
+                Integer.toString(round),
+                Integer.toString(attempt),
+                cursor_type,
+                Double.toString(poll_rate)
+        };
+
+        try {
+            database.insertIntoTable(tracesTable, values);
         }
         catch (SQLException ex) {
             LogEntryException.throwLogFailedException();
